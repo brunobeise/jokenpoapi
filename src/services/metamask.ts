24c9b0d74c3272ecc22351380e8038715831b243
brunobeise <bruno_beise@hotmail.com>
2023-05-09 22:34:16 -0300

    disbale metamask

--- src/services/metamask.ts
+++ src/services/metamask.ts
@@ -3,11 +3,7 @@ const Web3 = require("web3");
 // Cria a instância do Web3 utilizando o provider local do Ethereum
 const web3 = new Web3("https://data-seed-prebsc-1-s1.binance.org:8545/");
 
-// Testa a conexão com o nó
-web3.eth.net
-  .isListening()
-  .then(() => console.log("Conexão com o nó estabelecida com sucesso!"))
-  .catch((error: any) => console.error(`Falha ao conectar com o nó: ${error}`));
+// Testa a conexão com o n
 
 export async function monitoringByHash(txHash: string) {
   console.log("monitorando..." + txHash);
