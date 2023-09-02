cfa6306ff6371d178996ddd5f981cdc26ca177bb
brunobeise <bruno_beise@hotmail.com>
2023-09-02 20:23:51 -0300

    remove dist

--- dist/src/utils/sort.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sortResultPlay() {
-    const jogadas = ["pedra", "papel", "tesoura"];
-    const indiceSorteado = Math.floor(Math.random() * jogadas.length);
-    return jogadas[indiceSorteado];
-}
-exports.default = sortResultPlay;
